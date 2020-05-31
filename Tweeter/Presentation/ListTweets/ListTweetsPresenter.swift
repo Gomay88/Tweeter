f78a88ad36b3f5b532c515a988e04eb88ccdf220
Víctor Jiménez <gomi88@hotmail.es>
2020-05-31 17:35:26 +0200

    Load tweets every 5 seconds

--- Tweeter/Presentation/ListTweets/ListTweetsPresenter.swift
+++ Tweeter/Presentation/ListTweets/ListTweetsPresenter.swift
@@ -4,6 +4,7 @@ import Foundation
 protocol ListTweetsPresenter {
     func setView(view: ListTweetsViewController)
     func logInTweeter()
+    func loadTweets()
     func numberOfTwits() -> Int
     func twitAuthorForRow(row: Int) -> String
     func twitTextForRow(row: Int) -> String
@@ -17,7 +18,9 @@ class ListTweetsPresenterDefault: ListTweetsPresenter {
     private let accessTokenInteractor: AccessTokenInteractor = AccessTokenInteractorDefault()
     private let twitsInteractor: GetTweetsInteractor = GetTweetsInteractorDefault()
     
+    private var timer = Timer()
     private var twits: [Twit] = []
+    private var isLogged = false
     
     private weak var view: ListTweetsViewController?
     
@@ -39,20 +42,32 @@ class ListTweetsPresenterDefault: ListTweetsPresenter {
         }
     }
     
+    @objc func loadTweets() {
+        view?.startSpinner()
+        if self.isLogged {
+            self.twitsInteractor.execute { (twits) in
+                self.twits = twits
+                self.view?.endSpinner()
+                self.view?.loadData()
+                DispatchQueue.main.async {
+                    self.restartTimer()
+                }
+            }
+        }
+    }
+    
     @objc func didLogInTwitter() {
         view?.dismissSafari(completion: {
             self.view?.startSpinner()
             self.accessTokenInteractor.execute { (hasLoggedIn) in
                 if hasLoggedIn {
-                    self.twitsInteractor.execute { (twits) in
-                        self.twits = twits
-                        self.view?.endSpinner()
-                        self.view?.loadData()
-                    }
+                    self.isLogged = hasLoggedIn
+                    self.loadTweets()
                 } else {
                     self.view?.endSpinner()
                     self.view?.showError(error: RepositoryError.serverError)
                 }
+                
             }
         })
     }
@@ -75,5 +90,10 @@ class ListTweetsPresenterDefault: ListTweetsPresenter {
     
     func didSelectTwit(row: Int) {
         view?.navigateToTwitDetail(twit: twits[row])
+    }
+    
+    private func restartTimer() {
+        timer.invalidate()
+        timer = Timer.scheduledTimer(timeInterval: 5.0, target: self, selector: #selector(self.loadTweets), userInfo: nil, repeats: true)
     }
 }
